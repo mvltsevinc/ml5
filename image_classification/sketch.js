24d5104cb0c32318031aa3629a0a56909cb47c3d
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-09-08 22:53:09 +0300

    Video Classification

--- image_classification/sketch.js
+++ image_classification/sketch.js
@@ -1,38 +1,44 @@
 let mobilenet;
-let puffin;
+//let puffin;
+let video;
+let label='';
 
 function modelReady() {
   console.log("Model is ready!");
-  mobilenet.predict(puffin, gotResults);
+  mobilenet.predict(gotResults);
 }
 
 function gotResults(error, results) {
   if (error) {
     console.error(error);
   } else {
-    console.log(results);
-    let label = results[0].className;
-    let prop = results[0].probability * 100;
-    fill(0);
-    textSize(64);
-    text(label, 10, height - 100);
-    createP(label);
-    createP(prop);
+    //console.log(results);
+    label = results[0].className;
+    mobilenet.predict(gotResults);
   }
 }
 
-function imageReady() {
-  image(puffin, 0, 0, width, height);
-}
+// function imageReady() {
+//   image(puffin, 0, 0, width, height);
+// }
 
 function setup() {
   //createCanvas(windowWidth, windowHeight);
-  createCanvas(640, 480);
-  puffin = createImg("images/puffin.jpg", imageReady);
-  puffin.hide();
+  createCanvas(640, 550);
+  //puffin = createImg("images/puffin.jpg", imageReady);
+  //puffin.hide();
+  video = createCapture(VIDEO);
+  video.hide();
   background(0);
 
-  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+  //mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+  mobilenet = ml5.imageClassifier("MobileNet", video, modelReady);
 }
 
-function draw() {}
+function draw() {
+  background(0);
+  image(video, 0, 0);
+  fill(255);
+  textSize(32);
+  text(label, 10, height - 20);
+}
